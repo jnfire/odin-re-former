e14587eab13cb1a43f322a50121b24df03b17da8
Javier Nicolás Pérez Mesa <mesafilms@gmail.com>
2026-09-07 11:41:33 +0200

    Add user model, controller, and routes for user creation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'User was successfully created.'
+
+    else
+      render :new
+
+
+    end
+  end
+end
